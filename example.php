d481eabcf3e051f97365266653e18deb6383bdc0
lewsid <chris@bluehousegroup.com>
2013-07-06 13:42:35 -0400

    cleanup

--- example.php
+++ example.php
@@ -8,5 +8,3 @@ $raw_post = file_get_contents("php://input");
 $server_array = $_SERVER;
 
 $request = ChargifyWebhookHelper::grabHook($shared_key, $post_array, $raw_post, $server_array);
-
-echo '<pre>'; print_r($request); die();
