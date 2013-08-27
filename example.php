b0cdc3a06cf74da51ba6ebd3874aa5b3c14006cb
Christopher Lewis <lewsid@lewsid.com>
2013-08-26 20:37:29 -0400

    Updated doc to be a smidgen more helpful

--- example.php
+++ example.php
@@ -2,9 +2,10 @@
 
 include "lib/ChargifyWebhookHelper.php";
 
-$shared_key = "VwubnkmwIth4FGkRfu";
+$shared_key = "set_inside_chargify"; //http://docs.chargify.com/webhooks#finding-your-site-shared-key
 $post_array = $_POST;
 $raw_post = file_get_contents("php://input");
 $server_array = $_SERVER;
 
+//returns array('event' => string, 'payload' => array)
 $request = ChargifyWebhookHelper::grabHook($shared_key, $post_array, $raw_post, $server_array);
