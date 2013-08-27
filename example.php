0268f4a9d7291f76492fb0bb115ece5666ecbe7b
Christopher Lewis <lewsid@lewsid.com>
2013-08-26 20:53:10 -0400

    More sample stuff

--- example.php
+++ example.php
@@ -9,3 +9,14 @@ $server_array = $_SERVER;
 
 //returns array('event' => string, 'payload' => array)
 $request = ChargifyWebhookHelper::grabHook($shared_key, $post_array, $raw_post, $server_array);
+
+switch($_POST['event'])
+{
+    case 'renewal_failure':
+      //Handle account renewal failure, set flag on user account that should redirect them to form
+      $acoount_id = $_POST['payload']['subscription']['customer']['reference'];
+      break;
+      
+    case 'renewal_success:':
+      //etc...
+}
